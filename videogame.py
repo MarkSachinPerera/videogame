26db7c2371142ea3209260d9236a73368b7b0aa5
MarkSachinPerera <marksachin12@gmail.com>
2020-01-27 20:14:05 -0500

    saves

--- videogame.py
+++ videogame.py
@@ -1,11 +1,38 @@
 #!/usr/bin/python3
 
-from tkinter import *
+# from tkinter import *
+import random
+
+my_board = [ ('0', '0', '0', '0', '0', '0', '0'),
+             ('0', '1', '1', '1', '1', '1', '0'),
+             ('0', '1', '1', 'x', '1', '1', '0'),
+             ('0', '1', '1', '1', '1', '1', '0'),
+             ('0', '0', '0', '0', '0', '0', '0')]
+
+HEIGHT = 5
+WIDTH = 7
 
 
-my_board = [ (0, 0, 0, 0, 0, 0, 0),
-             (0, 1, 1, 1, 1, 1, 0),
-             (0, 1, 1, 0, 1, 1, 0),
-             (0, 1, 1, 1, 1, 1, 0),
-             (0, 0, 0, 0, 0, 0, 0)]
+def printBoard():
+    print(" ---" * len(my_board[0]))
 
+    for i in my_board:
+        print("|", end=' ')
+        for j in i:
+            print("%s |" % j, end=' ')
+        print("")
+        print(" ---" * len(my_board[0]))
+
+def main():
+    print("Welcome to my game!\n")
+    print("0 - means a wall")
+    print("1 - means a path to walk on")
+    print("x - is the user")
+    print("Use the arrow keys to move around")
+
+    printBoard()
+
+
+
+if __name__ == '__main__':
+    main()
