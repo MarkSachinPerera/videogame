e1678932fb465a3fc6c93c6765ef472538b085a4
MarkSachinPerera <marksachin12@gmail.com>
2020-02-03 21:49:51 -0500

    draws empty board

--- videogame.py
+++ videogame.py
@@ -24,33 +24,31 @@ def printBoard():
 
 
 class boardGame(Frame):
-    width = 6
-    height = 6
 
     # @property
     # def canvas_size(self):
     #     return(self.width * square_size, self.height * square_size)
 
     def __init__(self, parent, square_size=30):
+        self.width = 6
+        self.height = 6
         canvas_width = self.width * square_size
         canvas_height = self.height * square_size
 
         self.parent = parent
-        self.gif1 = PhotoImage(file='sample-gif-2.gif')
+        # self.S = PhotoImage(file='sample-gif-2.gif')
 
         Frame.__init__(self, parent)
 
-        self.canvas = Canvas(self, width=canvas_width, height=canvas_height)
-        self.canvas.create_image(480, 480, image=self.gif1, anchor=NW)
+        self.canvas = Canvas(self, width=canvas_width, height=canvas_height, bg="white")
+        # self.canvas.create_image(480, 480, image=self.gif1, anchor=NW)
         self.canvas.bind("<Key>", self.key)
         print("Use the arrow keys to move around")
         self.canvas.bind("<Button-1>", self.click)
 
         self.canvas.pack(side="top", fill="both", anchor="c", expand=True)
-        
-        self.canvas.pack()
 
-        self.canvas.focus_set()
+        self.canvas.focus_set()  # This will send pressed keys
 
         '''
         fill − Determines whether widget fills any extra space allocated to it by the packer, 
@@ -58,14 +56,22 @@ class boardGame(Frame):
         Y (fill only vertically), or BOTH (fill both horizontally and vertically).
         '''
 
+    def drawBoard(self,square_size=30):
+        for row in range(self.width):
+            for col in range(self.height):
+                x1 = col * square_size
+                y1 = (5-row) * square_size
+                x2 = x1 + square_size
+                y2 = y1 + square_size
+                self.canvas.create_rectangle(x1, y1, x2, y2, outline="black", fill="dark gray", tags="square")
+
+        self.canvas.tag_lower("square")
+
     def key(self, event):
         print("pressed ")
-        self.canvas.focus_set()
-
 
     def click(self, event):
         print("clicked")
-
 
 
 def main():
@@ -78,7 +84,8 @@ def main():
     mywindow = Tk()  # main window that i will be editing
 
     board = boardGame(mywindow)
-    board.pack(side="top", fill="both", expand="true")
+    board.pack(side="top", fill="both", expand="true", padx=4, pady=4)
+    board.drawBoard()
 
     mywindow.mainloop()
 
